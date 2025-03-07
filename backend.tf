59bac48e7f839cc9b7633851d9c445e716d41012
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2025-03-07 17:18:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
-    storage_account_name = "techtutorialswithpiyush"
-    container_name       = "prod-tfstate"
+    resource_group_name  = "day7rg"
+    storage_account_name = "sadfghjkldsfghj"
+    container_name       = "container"
     key                  = "prod.terraform.tfstate"
   }
 }
